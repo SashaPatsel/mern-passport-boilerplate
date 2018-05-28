80f179fa7263a5fa9bb6df06f0d6e7e2f255074a
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-05-28 11:59:36 -0700

    create landing page

--- README.md
+++ README.md
@@ -39,4 +39,4 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 
 ## Acknowledgments
 
-* 
+* Trilogy Ed
---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import API from "../../utils/API"
+import { Link } from "react-router-dom";
+import "./landing.css"
+
+class Landing extends Component {
+
+  render () {
+    return (
+      <div>
+        <div>
+          <form>
+            
+          </form>  
+        </div>  
+      </div>
+    )
+  }
+
+}
