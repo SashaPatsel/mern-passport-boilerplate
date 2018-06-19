969125b2b6092eead792adb7b00427fe95c28102
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-06-18 18:06:57 -0700

    add proxy container for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-import Landing from "./pages/Landing/Landing.js";
-import Home from "./pages/Home/Home.js";
-import SP from "./components/SP/SP.js"
+import ProxyContainer from "./ProxyContainer"
 
 const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch>
-      {/* <SP/> */}
-    </div>
-
-  </Router>
+  <ProxyContainer/>
 );
 
 export default App;
