8b40b3255e95e9e3729dca63fa907c9df016d6ed
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-06-24 10:48:36 -0700

    eliminate extra space on side

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,17 +4,19 @@ import "./nav.css"
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark mern-nav">
-      <div className="col-md-2">
+  {/* <div className="row">  */}
+      {/* <div className="col-md-2"> */}
         <a className="navbar-brand" href="/">
           MERN w/ Passport!
         </a>
-      </div>
-      <div className="col-md-5"></div>
-      <div className="col-md-5">
+      {/* </div> */}
+      {/* <div className="col-md-5"></div> */}
+      {/* <div className="col-md-9"> */}
         <div className="right-align">
           <SignIn />
         </div>
-      </div>
+      {/* </div> */}
+      {/* </div> */}
   </nav>
 );
 
